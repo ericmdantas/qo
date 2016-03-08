18ff15898ce58fd734a471a9fdcfd02dd4d35aea
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-03-07 23:18:50 -0300

    ..

--- qo.go
+++ qo.go
@@ -6,61 +6,43 @@ import (
 )
 
 const (
-	MAX int = 9999999
+	max int = 10000000
 )
 
 type qo struct {
-	list []interface{}
+	list []int
 }
 
-func (q *qo) push(d interface{}) {
-	q.list = append(q.list, d)
-}
-
-func (q *qo) unshift(d interface{}) {
-	q.list = append(q.list, d)
+func (q *qo) push(i int) {
+	q.list = append(q.list, i)
 }
 
 func (q *qo) pop() {
-	q.list = q.list[1:]
-}
-
-func (q *qo) shift() {
 	q.list = q.list[:len(q.list)-1]
-}
-
-func logAdd(s string, cb func(info interface{}), list []interface{}) {
-	t := time.Now()
-
-	fmt.Println(s)
-
-	for i := 0; i < MAX; i++ {
-		cb(i)
-	}
-
-	fmt.Println(time.Since(t))
-	fmt.Println(len(list))
-}
-
-func logRemove(s string, cb func(), list []interface{}) {
-	t := time.Now()
-
-	fmt.Println(s)
-
-	for i := 0; i < MAX; i++ {
-		cb()
-	}
-
-	fmt.Println(time.Since(t))
-	fmt.Println(len(list))
 }
 
 func main() {
 	q := qo{}
 
-	logAdd("push", q.push, q.list)
-	logAdd("unshift", q.unshift, q.list)
+	queue := q.list
 
-	logRemove("pop", q.pop, q.list)
-	logRemove("shift", q.pop, q.list)
+	start := time.Now()
+	for i := 0; i < max; i++ {
+		q.push(i)
+	}
+
+	elapsed := time.Since(start)
+
+	fmt.Println(elapsed)
+
+	start = time.Now()
+
+	for i := 0; i < max; i++ {
+		q.pop()
+	}
+
+	elapsed = time.Since(start)
+
+	fmt.Println(elapsed)
+	fmt.Println(queue)
 }
